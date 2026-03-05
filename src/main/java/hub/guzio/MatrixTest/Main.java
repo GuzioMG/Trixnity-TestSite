3b22b0fd5e9527b77af500f60906f8cfbba76bf1
Guzio <guzekk@protonmail.com>
2026-03-06 00:55:16 +0100

    a simple ping-loop
    Holy fuck, I forgot how verbose Java gets.
    JS: fetch() / Java: TWO OBJECTS, BODY ENCODERS, DECODERS, gówna na kiju

--- src/main/java/hub/guzio/MatrixTest/Main.java
+++ src/main/java/hub/guzio/MatrixTest/Main.java
@@ -8,10 +8,30 @@ import hub.guzio.MatrixTest.cannedHandlers.UnknownProtocol;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     static void main() throws IOException {
         var sv = HttpServer.create(new InetSocketAddress(8080), 1000);
+        var in = new Scanner(System.in);
+        System.out.print("Enter AppService Token: ");
+        var cli = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build();
+        HttpRequest rq;
+
+        try { rq = HttpRequest.newBuilder(new URI("https://api.chat.guziohub.ovh/_matrix/client/v1/appservice/javatest/ping"))
+                .header("Authorization", "Bearer "+in.next())
+                .POST(HttpRequest.BodyPublishers.ofString("{}")).build();
+        } catch (Throwable e){
+            System.err.print("Wrong token!"); //This is absolutely not what happens, but it's a nice'n'simple error message.
+            cli.close();
+            return;
+        }
 
         //Core endpoints
         sv.createContext("/_matrix/app/v1/transactions/", new MainHandler());
@@ -30,5 +50,21 @@ public class Main {
 
         sv.start();
         System.out.println("Started!");
+        while (true){
+            try {
+                if (Objects.equals(in.next(), "q")) {
+                    System.out.print("Exiting...");
+                    sv.stop(5);
+                    cli.shutdownNow();
+                    return;
+                }
+                HttpResponse<String> body = cli.send(rq, HttpResponse.BodyHandlers.ofString());
+                System.out.println("Sent request.");
+                System.out.print(body.body());
+            }
+            catch (Throwable e) {
+                e.printStackTrace(System.err);
+            }
+        }
     }
 }
